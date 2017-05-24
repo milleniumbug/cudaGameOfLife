9786ccaa8c5b3f7ccc57982f5f59d47c16c39b35
milleniumbug <wojas1222@gmail.com>
2017-05-24 06:21:33 +0200

    Initial version

---
+++ CUDA_Projekt_Sem08/cudaUtils.cuh
@@ -0,0 +1,79 @@
+#pragma once
+#include <memory>
+#include <cassert>
+#include <type_traits>
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+void reportCudaError(cudaError_t errorCode)
+{
+	assert(errorCode == cudaSuccess);
+}
+
+struct CudaDeleter
+{
+	template<typename T>
+	void operator()(T* ptr)
+	{
+		// lol, broken on VS2015
+		//static_assert(std::is_trivially_destructible<T>::value, "must be trivially destructible");
+		cudaFree(ptr);
+	}
+};
+
+template<typename T>
+std::unique_ptr<T, CudaDeleter> cudaMakeUniqueArray(std::size_t count)
+{
+	typedef typename std::unique_ptr<T, CudaDeleter>::element_type element_type;
+	// lol, broken on VS2015
+	//static_assert(std::is_trivially_destructible<element_type>::value, "must be trivially destructible");
+	void* untyped;
+	cudaError_t cudaStatus;
+	cudaStatus = cudaMalloc(&untyped, count * sizeof(element_type));
+	reportCudaError(cudaStatus);
+	cudaStatus = cudaMemset(untyped, 0, count * sizeof(element_type));
+	reportCudaError(cudaStatus);
+	auto typed = static_cast<element_type*>(untyped);
+	return std::unique_ptr<T, CudaDeleter>(typed);
+}
+
+template<typename T>
+class SynchronizedPrimitiveBuffer
+{
+	std::unique_ptr<T[]> hostMemory;
+	std::unique_ptr<T[], CudaDeleter> deviceMemory;
+	std::size_t size_;
+public:
+	SynchronizedPrimitiveBuffer(std::size_t size) :
+		hostMemory(std::make_unique<T[]>(size)),
+		deviceMemory(cudaMakeUniqueArray<T[]>(size)),
+		size_(size)
+	{
+		
+	}
+
+	void toDevice()
+	{
+		reportCudaError(cudaMemcpy(deviceMemory.get(), hostMemory.get(), size_ * sizeof(T), cudaMemcpyHostToDevice));
+	}
+
+	void toHost()
+	{
+		reportCudaError(cudaMemcpy(deviceMemory.get(), hostMemory.get(), size_ * sizeof(T), cudaMemcpyDeviceToHost));
+	}
+
+	std::size_t size() const
+	{
+		return size_;
+	}
+	
+	T& operator[](std::size_t pos)
+	{
+		return hostMemory[pos];
+	}
+
+	const T& operator[](std::size_t pos) const
+	{
+		return hostMemory[pos];
+	}
+};
--- CUDA_Projekt_Sem08/kernel.cu
+++ CUDA_Projekt_Sem08/kernel.cu
@@ -3,41 +3,147 @@
 #include "device_launch_parameters.h"
 
 #include <stdio.h>
+#include <cassert>
+#include <array>
+#include <iostream>
+#include "cudaUtils.cuh"
+
+__device__ int wrap(int in, int wrapCounter)
+{
+	return (in + wrapCounter * 2) % wrapCounter;
+}
+
+// in < 0: -1
+// in >= 0 && in < wrapCounter: 0
+// in >= wrapCounter: 1
+__device__ int robert(int in, int wrapCounter)
+{
+	if(in < 0)
+		return -1;
+	if(in >= wrapCounter)
+		return 1;
+	return 0;
+}
+
+__device__ bool rule(bool current, int neighbourCount)
+{
+	if(neighbourCount == 2)
+		return current;
+	else if(neighbourCount == 3)
+		return true;
+	else
+		return false;
+}
+
+const int maxNeighbourCount = 8;
+const int maxNeighbourAndSelfCount = maxNeighbourCount + 1;
+const int central = 4;
+const int upOrDown = 2;
+const int leftOrRight = 1;
+
+__global__ void nextGeneration(bool* next_generation, const bool* const* surrounding, int dim, bool* out)
+{
+	int x = threadIdx.x + blockIdx.x * blockDim.x;
+	int y = threadIdx.y + blockIdx.y * blockDim.y;
+	if(x >= dim || y >= dim)
+		return;
+
+	int neighbourCount =
+		surrounding[central + robert(x - 1, dim) * leftOrRight + robert(y - 1, dim) * upOrDown][wrap(x - 1, dim) + wrap(y - 1, dim)*dim] +
+		surrounding[central +                                    robert(y - 1, dim) * upOrDown][x +                wrap(y - 1, dim)*dim] +
+		surrounding[central + robert(x + 1, dim) * leftOrRight + robert(y - 1, dim) * upOrDown][wrap(x + 1, dim) + wrap(y - 1, dim)*dim] +
+
+		surrounding[central + robert(x - 1, dim) * leftOrRight                                ][wrap(x - 1, dim) + y*dim] +
+		surrounding[central + robert(x + 1, dim) * leftOrRight                                ][wrap(x + 1, dim) + y*dim] +
+
+		surrounding[central + robert(x - 1, dim) * leftOrRight + robert(y + 1, dim) * upOrDown][wrap(x - 1, dim) + wrap(y + 1, dim)*dim] +
+		surrounding[central +                                    robert(y + 1, dim) * upOrDown][x +                wrap(y + 1, dim)*dim] +
+		surrounding[central + robert(x + 1, dim) * leftOrRight + robert(y + 1, dim) * upOrDown][wrap(x + 1, dim) + wrap(y + 1, dim)*dim];
+
+	next_generation[x + y*dim] = rule(surrounding[central][x + y*dim], neighbourCount);
+
+	if(x == 0 && neighbourCount > 0)
+		out[central - leftOrRight] = true;
+	if(x == dim - 1 && neighbourCount > 0)
+		out[central + leftOrRight] = true;
+	if(y == 0 && neighbourCount > 0)
+		out[central - upOrDown] = true;
+	if(y == dim - 1 && neighbourCount > 0)
+		out[central + upOrDown] = true;
+}
+
+int main()
+{
+	int dim = 64;
+	int bufsize = dim*dim;
+	dim3 threadsPerBlock(16, 16);
+	dim3 dimensions(dim/threadsPerBlock.x, dim/threadsPerBlock.y);
+
+	auto hostCentral = std::make_unique<bool[]>(bufsize);
+	auto cudaCentral = cudaMakeUniqueArray<bool[]>(bufsize);
+	auto hostBorderCheck = std::make_unique<bool[]>(maxNeighbourAndSelfCount);
+	auto cudaBorderCheck = cudaMakeUniqueArray<bool[]>(maxNeighbourAndSelfCount);
+
+	auto hostSource = std::make_unique<bool[]>(bufsize);
+	// create a glider
+	hostSource[11 + 10 * dim] = true;
+	hostSource[12 + 11 * dim] = true;
+	hostSource[10 + 12 * dim] = true;
+	hostSource[11 + 12 * dim] = true;
+	hostSource[12 + 12 * dim] = true;
+	std::array<std::unique_ptr<bool[], CudaDeleter>, maxNeighbourAndSelfCount> cudaSurroundingPtrs =
+	{
+		cudaMakeUniqueArray<bool[]>(bufsize),
+		cudaMakeUniqueArray<bool[]>(bufsize),
+		cudaMakeUniqueArray<bool[]>(bufsize),
+		cudaMakeUniqueArray<bool[]>(bufsize),
+		cudaMakeUniqueArray<bool[]>(bufsize),
+		cudaMakeUniqueArray<bool[]>(bufsize),
+		cudaMakeUniqueArray<bool[]>(bufsize),
+		cudaMakeUniqueArray<bool[]>(bufsize),
+		cudaMakeUniqueArray<bool[]>(bufsize),
+	};
+	reportCudaError(cudaMemcpy(cudaSurroundingPtrs[central].get(), hostSource.get(), bufsize * sizeof cudaSurroundingPtrs[central][0], cudaMemcpyHostToDevice));
+	
+	auto hostCudaSurrounding = std::make_unique<bool*[]>(maxNeighbourAndSelfCount);
+	auto cudaCudaSurrounding = cudaMakeUniqueArray<bool*[]>(maxNeighbourAndSelfCount);
+	for(std::size_t i = 0; i < maxNeighbourAndSelfCount; ++i)
+	{
+		hostCudaSurrounding[i] = cudaSurroundingPtrs[i].get();
+	}
+	reportCudaError(cudaMemcpy(cudaCudaSurrounding.get(), hostCudaSurrounding.get(), maxNeighbourAndSelfCount * sizeof cudaCudaSurrounding[0], cudaMemcpyHostToDevice));
+
+	reportCudaError(cudaMemcpy(cudaCentral.get(), hostCentral.get(), bufsize * sizeof cudaCentral[0], cudaMemcpyHostToDevice));
+	nextGeneration <<< dimensions, threadsPerBlock >>>(cudaCentral.get(), cudaCudaSurrounding.get(), dim, cudaBorderCheck.get());
+	reportCudaError(cudaMemcpy(hostCentral.get(), cudaCentral.get(), bufsize * sizeof cudaCentral[0], cudaMemcpyDeviceToHost));
+
+	reportCudaError(cudaMemcpy(hostBorderCheck.get(), cudaBorderCheck.get(), maxNeighbourAndSelfCount * sizeof hostBorderCheck[0], cudaMemcpyDeviceToHost));
+
+	for(int j = 0; j < dim; ++j)
+	{
+		for(int i = 0; i < dim; ++i)
+		{
+			std::cout << (hostCentral[j*dim + i] ? "X" : " ");
+		}
+		std::cout << "\n";
+	}
+	std::cout << "\n";
+	for(int i = 0; i < maxNeighbourAndSelfCount; ++i)
+	{
+		if(hostBorderCheck[i])
+			std::cout << i;
+		else
+			std::cout << " ";
+	}
+	std::cout << "\n";
+}
 
 cudaError_t addWithCuda(int *c, const int *a, const int *b, unsigned int size);
 
 __global__ void addKernel(int *c, const int *a, const int *b)
 {
-    int i = threadIdx.x;
-    c[i] = a[i] + b[i];
-}
-
-int main()
-{
-    const int arraySize = 5;
-    const int a[arraySize] = { 1, 2, 3, 4, 5 };
-    const int b[arraySize] = { 10, 20, 30, 40, 50 };
-    int c[arraySize] = { 0 };
-
-    // Add vectors in parallel.
-    cudaError_t cudaStatus = addWithCuda(c, a, b, arraySize);
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "addWithCuda failed!");
-        return 1;
-    }
-
-    printf("{1,2,3,4,5} + {10,20,30,40,50} = {%d,%d,%d,%d,%d}\n",
-        c[0], c[1], c[2], c[3], c[4]);
-
-    // cudaDeviceReset must be called before exiting in order for profiling and
-    // tracing tools such as Nsight and Visual Profiler to show complete traces.
-    cudaStatus = cudaDeviceReset();
-    if (cudaStatus != cudaSuccess) {
-        fprintf(stderr, "cudaDeviceReset failed!");
-        return 1;
-    }
-
-    return 0;
+	int i = threadIdx.x;
+	c[i] = a[i] + b[i];
 }
 
 // Helper function for using CUDA to add vectors in parallel.
